3b0743d889b32ec636101dc9f988afa01208d61a
geminway92 <geminway@live.com>
2021-10-01 02:24:10 +0200

    Grid

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 import { createApp } from 'vue'
 import App from '../src/views/Portafolio.vue'
 import './css/styles.css'
 
 
 createApp(App).mount('#app')
-=======
-import { createApp } from 'vue'
-import App from '../src/views/Portafolio.vue'
-import './css/styles.css'
-
-
-createApp(App).mount('#app')
->>>>>>> 2c70237b1777e45e2254f1bd3c85948502b08bb3
