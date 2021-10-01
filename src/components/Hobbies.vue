0443aa36605f820d63526409335631a248abf3b4
geminway92 <geminway@live.com>
2021-10-01 02:03:07 +0200

    Corregido Grid

--- src/components/Hobbies.vue
+++ src/components/Hobbies.vue
@@ -1,75 +1,3 @@
-<<<<<<< HEAD
-<template>
-    <div class="card-hobbies">
-            <h2 class="title">Hobbies</h2>
-        <div class="hobbie">
-            <div class="hobbie__image-container">
-                <img src="../assets/images/photo-hobbie-cooking.jpg" alt="cooking" class="hobbie__image">
-            </div>
-            <h2 class="hobbie__title">Cocinar</h2>
-            <p class="hobbie__description">Me gusta cocinar todo tipo de platos, pero mi debilidad son los platos salados.</p>
-        </div>
-        <div class="hobbie">
-            <div class="hobbie__image-container">
-                <img src="../assets/images/photo-hobbie-gaming.jpg" alt="cooking" class="hobbie__image">
-            </div>
-            <h2 class="hobbie__title">Videojuegos</h2>
-            <p class="hobbie__description">Mi primer juego instalado en una PC fue The sims, desde ahí supe
-                que sería el principio de todo. </p>
-        </div>
-        <div class="hobbie">
-            <div class="hobbie__image-container">
-                <img src="../assets/images/photo-hobbie-cine.jpg" alt="cooking" class="hobbie__image">
-            </div>
-            <h2 class="hobbie__title">Cine / Series</h2>
-            <p class="hobbie__description"> Consumidora de casi todos los sitios de servicio en Streaming.</p>
-        </div>
-    </div>
-</template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style scoped >
-.card-hobbies {
-    background-color: var(--bg-card-color);
-    width: 250px;
-    padding: 1em;
-    border-radius: var(--border-radius);
-    transition: var(--transition);
-    margin: 0 0 1em 1em;
-
-}
-
-.title {
-    margin-bottom: 15px;
-}
-
-.hobbie__image-container {
-    width: 330px;
-    height: 150px;
-    overflow: hidden;
-}
-
-.hobbie__title {
-    font-size: 1em;
-    margin-top: 14px;
-}
-
-.hobbie__description {
-    font-size: 0.9em;
-}
-
-img.hobbie__image {
-    width: 100%;
-    height: 100%;
-    border-radius: var(--border-radius);
-}
-
-=======
 <template>
     <div class="card-hobbies">
             <h2 class="title">Hobbies</h2>
@@ -139,5 +67,4 @@ img.hobbie__image {
     border-radius: var(--border-radius);
 }
 
->>>>>>> 2c70237b1777e45e2254f1bd3c85948502b08bb3
 </style>
--- src/components/Toggle.vue
+++ src/components/Toggle.vue
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-<template>
-        <div @click="this.$emit('lightMode')" class="toggle">
-          <i 
-          class="fa toggle-toggle-icon"
-          :class="[!darkMode ? ' fa-sun' : 'fa-moon' ]"
-          >
-          </i>
-          <p class="toggle-theme-text">{{textMode}}</p>
-        </div>
-
-</template>
-
-<script>
-export default {
-    name: 'Toggle',
-    props: {
-        darkMode: {
-            type: Boolean,
-            default: false
-        },
-        textMode: {
-            type: String,
-            default: 'Light Mode',
-        },
-    },
-
-}
-</script>
-
-<style scoped>
-
-.toggle {
-    display: flex;
-    height: 60px;
-}
-
-.toggle-toggle-icon {
-    color: var(--text-color);
-    margin: 22px 0 0 10px;
-}
-
-.toggle-theme-text {
-    margin: 20px 0 0 10px
-}
-
-=======
 <template>
         <div @click="this.$emit('toggleTheme')" id="toggle-theme">
           <i id="toggle-icon" 
@@ -100,5 +53,4 @@ export default {
     margin-left: 5px;
 }
 
->>>>>>> 2c70237b1777e45e2254f1bd3c85948502b08bb3
 </style>
